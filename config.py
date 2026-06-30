0a8cd32f05a127b34231529f5edb398e790d454b
osvitakis-ux <osvita.kis@gmail.com>
2026-06-30 19:15:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,5 @@ import os
 BOT_TOKEN      = os.environ.get("BOT_TOKEN", "")
 ADMIN_CHAT_ID  = int(os.environ.get("ADMIN_CHAT_ID", 0))
 ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "konstanta2024")
+CRM_WEBHOOK_URL = os.environ.get("CRM_WEBHOOK_URL", "")  # URL вашої CRM для прийому лідів
+CRM_WEBHOOK_SECRET = os.environ.get("CRM_WEBHOOK_SECRET", "")  # опційний токен для перевірки запиту
